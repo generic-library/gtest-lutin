9d7e76cc171e4835b50232db9728cb77cf6dc46a
Edouard DUPIN <yui.heero@gmail.com>
2015-10-14 21:21:03 +0200

    [DEV] update new lutin 0.8.0

--- lutin_gtest.py
+++ lutin_gtest.py
@@ -1,15 +1,32 @@
 #!/usr/bin/python
 import lutin.module as module
 import lutin.tools as tools
+import os
+
+
+def get_type():
+	return "LIBRARY_STATIC"
 
 def get_desc():
-	return "gtest : google test interface"
+	return "google test interface"
 
-def get_license():
-	return "BSD 3 clauses"
+def get_licence():
+	return "BSD-3"
 
-def create(target):
-	my_module = module.Module(__file__, 'gtest', 'LIBRARY_STATIC')
+def get_compagny_type():
+	return "com"
+
+def get_compagny_name():
+	return "Google"
+
+def get_maintainer():
+	return ["mailing-list gtest <googletestframework@googlegroups.com>"]
+
+def get_version():
+	return [1,7,0]
+
+def create(target, module_name):
+	my_module = module.Module(__file__, module_name, get_type())
 	my_module.add_src_file([
 		'gtest/src/gtest-all.cc',
 		'gtest/src/gtest.cc',
@@ -22,19 +39,35 @@ def create(target):
 		'gtest/src/gtest-typed-test.cc'
 		])
 	my_module.add_header_file([
-		'gtest/include/gtest/gtest-death-test.h',
-		'gtest/include/gtest/gtest.h',
-		'gtest/include/gtest/gtest-message.h',
+		'gtest/include/gtest/gtest-typed-test.h',
 		'gtest/include/gtest/gtest-param-test.h',
-		'gtest/include/gtest/gtest_pred_impl.h',
-		'gtest/include/gtest/gtest-printers.h',
-		'gtest/include/gtest/gtest_prod.h',
-		'gtest/include/gtest/gtest-spi.h',
+		'gtest/include/gtest/gtest-message.h',
 		'gtest/include/gtest/gtest-test-part.h',
-		'gtest/include/gtest/gtest-typed-test.h'
-		], rm_path='gtest/include/')
-	my_module.add_path(tools.get_current_path(__file__)+"/gtest")
-	my_module.add_export_path(tools.get_current_path(__file__)+"/gtest/include/")
+		'gtest/include/gtest/gtest.h',
+		'gtest/include/gtest/gtest-param-test.h.pump',
+		'gtest/include/gtest/gtest-printers.h',
+		'gtest/include/gtest/gtest-death-test.h',
+		'gtest/include/gtest/gtest_pred_impl.h',
+		'gtest/include/gtest/gtest_prod.h',
+		'gtest/include/gtest/gtest-spi.h'
+		], destination_path='gtest')
+	my_module.add_header_file([
+		'gtest/include/gtest/internal/gtest-param-util-generated.h.pump',
+		'gtest/include/gtest/internal/gtest-type-util.h',
+		'gtest/include/gtest/internal/gtest-tuple.h',
+		'gtest/include/gtest/internal/gtest-param-util.h',
+		'gtest/include/gtest/internal/gtest-string.h',
+		'gtest/include/gtest/internal/gtest-linked_ptr.h',
+		'gtest/include/gtest/internal/gtest-tuple.h.pump',
+		'gtest/include/gtest/internal/gtest-param-util-generated.h',
+		'gtest/include/gtest/internal/gtest-death-test-internal.h',
+		'gtest/include/gtest/internal/gtest-port.h',
+		'gtest/include/gtest/internal/gtest-type-util.h.pump',
+		'gtest/include/gtest/internal/gtest-internal.h',
+		'gtest/include/gtest/internal/gtest-filepath.h'
+		], destination_path='gtest/internal')
+	my_module.add_path(os.path.join(tools.get_current_path(__file__), "gtest"))
+	my_module.add_path(os.path.join(tools.get_current_path(__file__), "gtest/include"))
 	return my_module
 
 
